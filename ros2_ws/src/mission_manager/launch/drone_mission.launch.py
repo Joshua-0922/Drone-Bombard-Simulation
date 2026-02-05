9f737bf7c4c1eb97078469df72de6021751fbe42
Joshua-0922 <nayoonho0922@snu.ac.kr>
2026-02-05 11:42:54 +0000

    launch

---
+++ ros2_ws/src/mission_manager/launch/drone_mission.launch.py
@@ -0,0 +1,36 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. Vision Node (CMake 패키지)
+        Node(
+            package='vision_detection',
+            # 중요: CMakeLists.txt 설정에 따라 .py가 붙을 수도, 안 붙을 수도 있습니다.
+            # 만약 "executable not found" 에러가 나면 '.py'를 붙여보세요.
+            executable='monocamera_xmarker_detector.py', 
+            name='xmarker_detector',
+            output='screen',
+            parameters=[
+                {'inference_rate': 10.0},
+                {'model_path': '/workspace/ros2_ws/yolo_workspace/runs/train/drone_bombard_train2/weights/best.pt'}
+            ]
+        ),
+
+        # 2. Mission Manager Node (Python 패키지)
+        Node(
+            package='mission_manager', # 패키지 이름 (setup.py의 package_name)
+            executable='mission_manager_node', # console_scripts에 적힌 이름
+            name='mission_manager',
+            output='screen'
+        ),
+
+        # 3. Drone Controller Node (Python 패키지)
+        Node(
+            package='drone_controller', # 패키지 이름
+            executable='controller',    # ★ setup.py에서 'controller =' 라고 정의했으므로 이게 실행 이름임
+            name='drone_controller',
+            output='screen'
+        ),
+    ])
